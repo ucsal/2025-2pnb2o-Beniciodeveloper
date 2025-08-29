14c03889509c6929bfe417f38053a8c279e88595
Lucas Benicio Santos <lucas.benicio@fbest.org.br>
2025-08-29 12:03:27 -0300

    aula 03

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,11 +1,24 @@
 package br.com.mariojp.solid.ocp;
 
+import java.util.Map;
+
 public class DiscountCalculator {
-    public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
-        }
+    private final Map<CustomerType, DiscountPolicy> discountPolicies;
+    
+    public DiscountCalculator() {
+        this.discountPolicies = Map.of(
+            CustomerType.REGULAR, new RegularCustomerDiscountPolicy(),
+            CustomerType.PREMIUM, new PremiumCustomerDiscountPolicy(),
+            CustomerType.PARTNER, new PartnerCustomerDiscountPolicy()
+        );
+    }
+    
+    public DiscountCalculator(Map<CustomerType, DiscountPolicy> discountPolicies) {
+        this.discountPolicies = discountPolicies;
+    }
+    
+    public double apply(double amount, CustomerType type) {
+        DiscountPolicy policy = discountPolicies.getOrDefault(type, new NoDiscountPolicy());
+        return policy.apply(amount);
     }
 }
